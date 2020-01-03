e4094df15f74fc2737c1ff3b05dd6d73b4ae9eb9
mitomartino <william.m.martino@gmail.com>
2020-01-02 20:25:11 -0500

    mail config

--- config/mail.js
+++ config/mail.js
@@ -22,17 +22,17 @@
 
 var config =
 {
-    email:    "mailer.d20server@gmail.com",
+    email:    "",
     authType: "xoauth2",
     authData:
     {
-        user:              "mailer.d20server@gmail.com",
-        displayName:       "D20 Server",
-        clientId:          "495012096978-po8ce79mj7q32017m4vjj9p6jbsdjrm8.apps.googleusercontent.com",
-        clientSecret:      "0aQB1mttQT2mhZpvWcAPcQTI",
-        authorizationCode: "4/s3tfhuKI4sWQtbiVmqCp-Qrf8owrj4tf8q1OgRwIvNU",
-        refreshToken:      "1/d6p0qO9ak-BvQatwkM7WRs_G4chv1D1I0QcNQ_jlC94",
-        accessToken:       "ya29.Ci-gA83FUJj6G96ig-N9bK0wajwdRuWDTfpjGPJEJX6szbYDPHKFooXFEy7hUsxr5w"
+        user:              "",
+        displayName:       "",
+        clientId:          "",
+        clientSecret:      "",
+        authorizationCode: "",
+        refreshToken:      "",
+        accessToken:       ""
     }
 };
 
